45bf09b8aac73cdf9033dd4515a6f48f830aee10
Ryan Mulligan <ryan@ryantm.com>
2025-02-19 15:51:00 -0800

    [security] npm audit fix
    Why
    ===
    * We had a dependabot report of problems with braces
    
    What changed
    ===
    * ran `npm audit fix`
    * fixed some parts of the tests (they still don't work)
    
    Test plan
    ===
    * Make a nodejs repl, pull in this branch as a package, confirm that
    repl auth works still

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,10 @@
 import express from 'express'
 import { getUserInfo } from '../lib/index.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.static('public'));
